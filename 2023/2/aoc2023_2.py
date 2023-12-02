5f3a065900ca0589eed9d5356665d0bdd36f00fe
Rok Krajnc <rok.krajnc@gmail.com>
2023-12-02 20:44:11 +0100

    2023 - 2.2

--- 2023/2/aoc2023_2.py
+++ 2023/2/aoc2023_2.py
@@ -5,8 +5,10 @@ NREDS = 12
 NGREENS = 13
 NBLUES = 14
 
+
 with open(INPUT_FILENAME, "r") as input_file:
   possible_games = []
+  sum_cube_power = 0
   for line in input_file:
     game, results = line.split(":")
     game_no = int(game[5:])
@@ -14,15 +16,20 @@ with open(INPUT_FILENAME, "r") as input_file:
     #print(line)
     results = results.split(";")
     possible_game = True
+    max_num_cubes = {"red":0, "green":0, "blue":0}
     for result in results:
       num_cubes = {"red":0, "green":0, "blue":0}
       cubes = result.split(",")
       for cube in cubes:
         num, color = cube.strip().split(" ")
         num_cubes[color] = num_cubes[color] + int(num)
+        if num_cubes[color] > max_num_cubes[color] : max_num_cubes[color] = num_cubes[color] 
       #print (num_cubes)
       if num_cubes["red"] > NREDS or num_cubes["green"] > NGREENS or num_cubes["blue"] > NBLUES:
         possible_game = False
     if possible_game : possible_games.append(game_no)
+    #print (max_num_cubes)
+    sum_cube_power += max_num_cubes["red"] * max_num_cubes["green"] * max_num_cubes["blue"]
   print("Possible games: ", possible_games)
   print("SUM: %d" % sum(possible_games))
+  print("SUM CUBE POWER: %d" % sum_cube_power)
